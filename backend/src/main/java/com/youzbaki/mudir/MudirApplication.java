b94d9a73805df9415f716c83c47ab39ba5022980
Mustafa Al-Youzbaki <47333242+yzbki@users.noreply.github.com>
2026-08-25 18:09:43 -0400

    Set up Java Spring Boot backend

---
+++ backend/src/main/java/com/youzbaki/mudir/MudirApplication.java
@@ -0,0 +1,13 @@
+package com.youzbaki.mudir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudirApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/youzbaki/mudir/MudirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youzbaki.mudir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
